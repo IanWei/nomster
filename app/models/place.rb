24eebc0185419e81cecb9177939246d6256795fd
IanWei <ikaeuu@gmail.com>
2017-11-16 11:27:19 +0000

    Geocoding address

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,10 @@
 class Place < ApplicationRecord
   paginates_per 1
   belongs_to :user
+
+  geocoded_by :address
+  after_validation :geocode
+  
   validates :name, :description, :address, presence: true
   validates :name, length: { minimum: 2}
 end
