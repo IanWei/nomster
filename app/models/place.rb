9b988ebb18b60e3ba992454d7d7280724dfcbf4f
IanWei <ikaeuu@gmail.com>
2017-11-16 10:59:55 +0000

    More validation

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,6 @@
 class Place < ApplicationRecord
   paginates_per 1
   belongs_to :user
-  validates :name, presence: true
+  validates :name, :description, :address, presence: true
+  validates :name, length: { minimum: 2}
 end
